c627ff871e46bd6e186e5f9dd3c67f475bdb24eb
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-07-11 15:01:58 +0200

    done

---
+++ solution.js
@@ -0,0 +1,36 @@
+//#1
+
+let determiner = 76;
+
+let x = 34;
+
+// determiner < 0 ? x < 0 : determiner >= 0 ? x >= 0 : ""
+
+if (determiner < 0) {
+  x < 0;
+} else if (determiner >= 0) {
+  x >= 0;
+}
+
+console.log(x);
+
+//#2
+
+let updater;
+
+if (determiner >= 0) {
+  updater >= 0;
+
+  let message = "Positive Integer";
+} else if (determiner < 0) {
+  updater = "Less than zero";
+  console.log(updater);
+}
+
+console.log(message);
+
+// If message is printed outside of the if statment it would return an error b/c the variable message is only defined inside the if statment and not outside. In other words, it doen't have a global visibility
+
+//#3
+
+// Ternary is used when there is one line of condition to check and if/else statement is used when we have multiple lines of conditions or when there is a complicated  code
